9c823e3baf5b72711a0d18c778077a2d19a7bb66
Gabriel <hello@gabrielrolino.com>
2023-10-04 17:29:13 +0100

    Added myScript.js

--- myScripts/myScripts.js
+++ myScripts/myScripts.js
@@ -1,1 +1,24 @@
-const svgElement = document.getElementById('animated-svg').contentDocument;
+window.onload = function () {
+  function initializeSVGAnimation() {
+    const svgObject = document.getElementById("animated-svg");
+
+    if (svgObject) {
+      const svgDoc = svgObject.contentDocument;
+
+      if (svgDoc) {
+        const pathElement = svgDoc.getElementById("path-id");
+
+        anime({
+          targets: pathElement,
+          stroke: "#FF0000",
+          duration: 4000,
+          easing: "easeOutExpo",
+        });
+      }
+    }
+  }
+
+  document
+    .getElementById("animated-svg")
+    .addEventListener("load", initializeSVGAnimation);
+};
